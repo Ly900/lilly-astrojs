397677c69d3239830323ca4b98c2ab95095ce6ba
Ly Nguyen <lynguyendc@gmail.com>
2024-03-31 15:12:37 -0400

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ly900.github.io/',
   base: '/lilly-astrojs',
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
--- src/components/UpvoteContent.tsx
+++ src/components/UpvoteContent.tsx
@@ -13,7 +13,7 @@ export const UpvoteContent = () => {
 						prevCount < MAX_COUNT ? prevCount + 1 : prevCount
 					);
 				}}
-				className="h-[80px] w-[80px] rounded-full border border-gray-500 bg-slate-900 p-4 text-center text-sm text-green-600 hover:scale-90 active:bg-slate-800"
+				className="h-[80px] w-[80px] rounded-full border border-gray-500 bg-white p-4 text-center text-sm text-red-400 hover:scale-90 active:bg-slate-800 transition-all"
 			>
 				<svg
 					className="upvote__icon"
@@ -32,7 +32,6 @@ export const UpvoteContent = () => {
 						d="M7 11c.889-.086 1.416-.543 2.156-1.057a22.323 22.323 0 0 0 3.958-5.084 1.6 1.6 0 0 1 .582-.628 1.549 1.549 0 0 1 1.466-.087c.205.095.388.233.537.406a1.64 1.64 0 0 1 .384 1.279l-1.388 4.114M7 11H4v6.5A1.5 1.5 0 0 0 5.5 19v0A1.5 1.5 0 0 0 7 17.5V11Zm6.5-1h4.915c.286 0 .372.014.626.15.254.135.472.332.637.572a1.874 1.874 0 0 1 .215 1.673l-2.098 6.4C17.538 19.52 17.368 20 16.12 20c-2.303 0-4.79-.943-6.67-1.475"
 					/>
 				</svg>
-				<p className="upvote__text">Upvote</p>
 			</button>
 
 			<div className="ml-3">
