9deeb776ed22fd3d5766093a457d5a0b76b853c1
Ly Nguyen <lynguyendc@gmail.com>
2024-03-31 14:54:09 -0400

    add react and add vote section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ly900.github.io/',
-	base: '/lilly-astrojs',
+  site: 'https://ly900.github.io/',
+  base: '/lilly-astrojs',
+  integrations: [react()]
 });
