3459a0dad741fbf6d47a1d0b0a7cb3991f83f856
Ly Nguyen <lynguyendc@gmail.com>
2024-03-31 14:59:43 -0400

    Change vote icon

--- src/components/UpvoteContent.tsx
+++ src/components/UpvoteContent.tsx
@@ -16,21 +16,23 @@ export const UpvoteContent = () => {
 				className="h-[80px] w-[80px] rounded-full border border-gray-500 bg-slate-900 p-4 text-center text-sm text-green-600 hover:scale-90 active:bg-slate-800"
 			>
 				<svg
-					fill="none"
-					className="mx-auto w-8"
-					stroke="currentColor"
-					strokeWidth="1.5"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
+					className="upvote__icon"
 					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					width="50"
+					height="50"
+					fill="none"
+					viewBox="0 0 24 24"
 				>
 					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-					></path>
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M7 11c.889-.086 1.416-.543 2.156-1.057a22.323 22.323 0 0 0 3.958-5.084 1.6 1.6 0 0 1 .582-.628 1.549 1.549 0 0 1 1.466-.087c.205.095.388.233.537.406a1.64 1.64 0 0 1 .384 1.279l-1.388 4.114M7 11H4v6.5A1.5 1.5 0 0 0 5.5 19v0A1.5 1.5 0 0 0 7 17.5V11Zm6.5-1h4.915c.286 0 .372.014.626.15.254.135.472.332.637.572a1.874 1.874 0 0 1 .215 1.673l-2.098 6.4C17.538 19.52 17.368 20 16.12 20c-2.303 0-4.79-.943-6.67-1.475"
+					/>
 				</svg>
-				Upvote
+				<p className="upvote__text">Upvote</p>
 			</button>
 
 			<div className="ml-3">
